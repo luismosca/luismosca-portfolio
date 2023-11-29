5f243342bf076934ff43a73e4bae86859f893133
luismosca <luismosca@gmail.com>
2023-11-28 23:42:24 -0300

    portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,53 +2,64 @@ import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
+import foto1 from '../../Images/foto1.jpg'
 
 const About = () => {
-  const { name, role, description, resume, social } = about
+  const { name, image, role, description, description2, resume, social } = about
 
   return (
     <div className='about center'>
+      <img
+        src={foto1}
+        alt='profile_picture'
+        style={{ width: 150, borderRadius: 15 }}
+      />
       {name && (
-        <h1>
+        <h1 style={{ marginTop: 15 }}>
           Hi, I am <span className='about__name'>{name}.</span>
         </h1>
       )}
 
       {role && <h2 className='about__role'>A {role}.</h2>}
       <p className='about__desc'>{description && description}</p>
-
-      <div className='about__contact center'>
+      <p className='about__desc'>{description2 && description2}</p>
+      <div className='about__contact center' style={{ display: 'block' }}>
         {resume && (
-          <a href={resume}>
+          <a href={resume} target='_blank' rel='noreferrer'>
             <span type='button' className='btn btn--outline'>
               Resume
             </span>
           </a>
         )}
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {social && (
+            <>
+              {social.github && (
+                <a
+                  href={social.github}
+                  aria-label='github'
+                  className='link link--icon'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <GitHubIcon />
+                </a>
+              )}
 
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                <GitHubIcon />
-              </a>
-            )}
-
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                <LinkedInIcon />
-              </a>
-            )}
-          </>
-        )}
+              {social.linkedin && (
+                <a
+                  href={social.linkedin}
+                  aria-label='linkedin'
+                  className='link link--icon'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <LinkedInIcon />
+                </a>
+              )}
+            </>
+          )}
+        </div>
       </div>
     </div>
   )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,22 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://luismosca.github.io/portfolio',
+  title: 'LEM',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Luis Moscarelli',
+  image: '../src/components/Images/Foto_cara_mia.jpg',
+  role: 'Full Stack Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Soy Licenciado en Sistemas y FullStack Developer, me dedico a la administración de Sistemas, Soporte Técnico y al desarrolo Web..',
+  description2:
+    'Con conocimientos en Desarrollo Full Stack Developer. Con Experiencia en Desarrollo WEB: Ej. diseñando soluciones con bases de datos Postgress, SQL y generando proyectos que me permitieron desarrollar habilidades utilizando mis Skills .',
+  resume: 'https://cvluismosca.tiiny.site/',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/in/luis-moscarelli-b5664143',
+    github: 'https://github.com/luismosca',
   },
 }
 
@@ -21,28 +24,39 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Project 1: PI DOGS',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Proyecto Individual de Henry acerca de las diferentes razas de Perros con sus caracterristicas principales de Peso, Temperamento y Vida',
+    stack: [
+      'Javascript, Node, React, Redux, Sequelize, Express, PostgresSQL',
+      'TypeScript',
+      'React',
+    ],
+    sourceCode: 'https://github.com/luismosca/PI-Dogs',
+    livePreview: 'https://github.com/luismosca/PI-Dogs',
   },
   {
-    name: 'Project 2',
+    name: 'Project 2: PF ByR',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Pagina Web para reposrtar personas desaparecidas y poder registrar sus carecteristicas y posbles avistajes',
+    stack: [
+      'Javascript, Node, React, Redux, Sequelize, Express, PostgresSQL',
+      'TypeScript',
+      'React',
+    ],
+    sourceCode: 'https://github.com/luismosca/portfolio',
+    livePreview: 'https://github.com/luismosca/portfolio',
   },
   {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    name: 'Project 3: OpenBootCamp',
+    description: 'Proyecto ejemplodel curso de Open BootCamp de javaScript',
+    stack: [
+      'Javascript, Node, React, Redux, Sequelize, Express, PostgresSQL',
+      'TypeScript',
+      'React',
+    ],
+    sourceCode: 'https://github.com/luismosca/OB-ProyectoFinal',
+    livePreview: 'https://github.com/luismosca/OB-ProyectoFinal',
   },
 ]
 
@@ -55,17 +69,16 @@ const skills = [
   'TypeScript',
   'React',
   'Redux',
-  'SASS',
   'Material UI',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Express',
+  'Sequelize',
+  'PostgressSQL',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'luismosca@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
